f58ba537a48d0b64d033f59922e14ddcfefdbe4c
Fábio A. Ferreira <fantonios@gmail.com>
2020-07-27 23:52:53 -0300

    Adicionando classe metodo construtor

---
+++ C++/MetodoContrutor.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+#include <string>
+using std::string;
+
+class GradeBook
+{
+public:
+  GradeBook(string name)
+  {
+    setCourseName(name);
+  }
+
+  void setCourseName(string name)
+  {
+    courseName = name;
+  }
+  string getCourseName()
+  {
+    return courseName;
+  }
+  void displayMessage()
+  {
+    cout << "Welcome to the grade book for\n" <<  getCourseName() << "!" << endl;
+  }
+
+private:
+  string courseName;
+};
+
+int main()
+{
+  GradeBook gradeBook1("CS101 Introduction to C++ Programming");
+  GradeBook gradeBook2("CS102 Data Structures in C++");
+
+  cout << "gradeBook1 created for course: " << gradeBook1.getCourseName() << "\ngradeBook2 created for course: " << gradeBook2.getCourseName() << endl;
+
+  return 0;
+}
