1d86cc177d717b60b8d8f16dd093c1abce6deaa6
Fábio Ferreira <fantonios@gmail.com>
2020-07-29 00:08:51 -0300

    Segue o Fluxo

---
+++ C++/AloFunc2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void digaAlo()
+{
+  cout << "That's all Folks" << endl;
+}
+void digaTchau()
+{
+  cout << "Good By!!!" << endl;
+}
+int main()
+{
+  digaAlo();
+  digaTchau();
+  return 0;
+}
