d387f18394f426f203b5500efe31353c5c397c34
Fabio Ferreira <fantonios@gmail.com>
2022-02-14 22:41:41 -0300

    cap02

--- 1ºbim/cap02_Algoritmos_Recursivos/exer_algoritmos_recursivos/rec_fat_if.cpp
+++ 1ºbim/cap02_Algoritmos_Recursivos/exer_algoritmos_recursivos/rec_fat_if.cpp
@@ -2,7 +2,7 @@
 
 /*
 
-Programa para calcula o fatorial de um número natura n;
+Programa para calcula o fatorial de um número natural n, utilizando if;
 
 */
 
@@ -11,11 +11,9 @@ using std::cin;
 using std::endl;
 
 int fat(int n){
-    int f = 1;
-    while (n > 0)
-        f *= n--;
-        printf("Valo de Fat: %d\n", f);
-    return f;
+    if (n == 0 || n == 1) return 1;
+        printf("Valo de Fat: %d\n", n);
+    return n*fat(n-1);
 }
 int main(){
     int numero = 5;
