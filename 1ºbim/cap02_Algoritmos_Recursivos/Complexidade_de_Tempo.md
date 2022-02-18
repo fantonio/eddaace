4867316edfb45c222cc7ed9ac0c750c217a4fcb9
Fabio Ferreira <fantonios@gmail.com>
2022-02-17 22:52:44 -0300

    cap02

---
+++ 1ºbim/cap02_Algoritmos_Recursivos/Complexidade_de_Tempo.md
@@ -0,0 +1,15 @@
+#### **Análise de complexidade de tempo**
+
+Seja T(n) o tempo de pior caso de fat(n):
+`Base: T(0) = T(1) = a`
+`Parte indutiva: T(n) = T(n-1) + b, n>1`
+
+Cálculos:
+``` 
+T(2) = T(1) + b = a + b
+T(3) = T(2) + b = a + 2b
+T(4) = T(3) + b = a + 3b
+
+Generalizando: T(n) = a + (n-1)b
+Portanto: T(n) = Θ(n)
+``` 
---
+++ 1ºbim/cap03_Estruturas_de_Dados_Elementares/exer_estrutura_de_dados/exer_pilha02.cpp
@@ -0,0 +1,29 @@
+#include <stack>
+#include <iostream>
+
+int main( )
+{
+   using namespace std;
+   // Declares stacks with default deque base container
+   stack <int> s1=10, s2;
+
+   s1.push( 1 );
+   s1.push( 2 );
+   s1.push( 3 );
+   s1.push( 4 );
+   s1.push( 5 );
+   s1.pop();
+
+   if ( s1.empty( ) )
+      cout << "The stack s1 is empty." << endl;
+   else
+      cout << "-> A pilha s1 não está vazia." << endl;
+      printf(" # O valor contigo na pilha é: %d\n", s1.top());
+
+   if ( s2.empty( ) )
+      cout << "-> A pilha s2 está vazia." << endl;
+   else
+      cout << "The stack s2 is not empty." << endl;
+}
+
+
